08ec1ffd289fbccf21e66f50cbe30f14af0c9a8b
shrinivas96 <shrinivasiyengar@live.com>
2024-03-09 15:33:26 +0100

    Genrates less random player positions  - Two new functions both making the player's movement less random  - Added a way to track in which direction the player went last time  - The distance the player covers in each time step can now be randomised

--- generate_ranges.py
+++ generate_ranges.py
@@ -16,6 +16,9 @@ class FieldAssets:
         # holds the player/sensors position
         self.playerPos = initPos
 
+        # holds the last chosen direction so that there is more chances that the player goes there the next time
+        self.lastQuadrant = None
+
         # assuming all functions will be run at 20 Hz,
         # AND given that avg player runs at 5 m/s,
         # the average distance a player will cover is
@@ -23,6 +26,9 @@ class FieldAssets:
 
         # noise for distorting all measurements
         self.noise = 0.3
+
+        # should the players speed be randomly changing?
+        self.randomSpeeds = False
 
         # position of all the sensors
         self.receiverPos = {
@@ -76,12 +82,140 @@ class FieldAssets:
             ]
         )
 
+    def lessRandomRunning(self) -> None:
+        """
+        This fucntion does the same job as whereAreYouRunning except it is an attempt 
+        to make the player run more in the same direction as before, yet preserving a fair bit of randomness.
+        """
+        # the player can go into one of these quadrants,
+        # depending on if these quadrants are close to the border
+        quadrants_available = [
+            [45, 135],
+            [135, 225],
+            [225, 315],
+            [315, 405],
+        ]  # (x-axis is 0 deg)
+
+        # based on last chosen quadrant, duplicate it for more chances to be selected
+        if self.lastQuadrant is not None:
+            quadrants_available += [self.lastQuadrant]*20
+
+        # current player position
+        x = self.playerPos[0]
+        y = self.playerPos[1]
+
+        # check which quadrant(s) can be removed based on distance from the border
+        # the remaining quadrants are the directions where the player can move
+        quads_to_be_ignored = []
+        if x < 0.25:
+            quads_to_be_ignored.append(1)
+        elif x > 99.25:
+            quads_to_be_ignored.append(3)
+        if y < 0.25:
+            quads_to_be_ignored.append(2)
+        elif y > 59.25:
+            quads_to_be_ignored.append(0)
+
+        # expand the rest of the quadrants to be randomly chosen out of
+        available_directions = []
+        for quad in quadrants_available:
+            if quad in quads_to_be_ignored:
+                continue
+            available_directions += list(range(quad[0], quad[1]))
+
+        # choose a direction where the player goes
+        direction_deg = random.choice(available_directions)
+        direction = direction_deg * (np.pi / 180)
+
+        # based on the chosen direction, mark it to be duplicated
+        for quad in quadrants_available:
+            if quad[0] < direction_deg < quad[1]:
+                self.lastQuadrant = quad
+                break
+
+        # can the player run at different speeds? makes the problem much harder
+        if self.randomSpeeds:
+            dist_covered = random.uniform(0, self.avgDistCovered)
+        else:
+            dist_covered = self.avgDistCovered
+
+        # this is the new player position for this time step
+        self.playerPos += np.array(
+            [
+                dist_covered * np.cos(direction),
+                dist_covered * np.sin(direction),
+            ]
+        )
+
+    def alternativeRunning(self):
+        """
+        This function is a way to update the player position less randomly. It gives a defined path to the player, 
+        in hopes that the LS solution will track better. It chooses a random direction at first and then goes more 
+        in that direction until the borders are reached.
+        """
+        # the player can go into one of these quadrants,
+        # depending on if these quadrants are close to the border
+        quadrants_available = [
+            [45, 135],
+            [135, 225],
+            [225, 315],
+            [315, 405],
+        ]  # (x-axis is 0 deg)
+
+        # current player position
+        x = self.playerPos[0]
+        y = self.playerPos[1]
+
+        # check which quadrant(s) can be removed based on distance from the border
+        # the remaining quadrants are the directions where the player can move
+        if x < 0.25:
+            quadrants_available.pop(1)
+        elif x > 99.25:
+            quadrants_available.pop(3)
+        if y < 0.25:
+            quadrants_available.pop(2)
+        elif y > 59.25:
+            quadrants_available.pop(0)
+
+        # if the last chosen quadrant has not been removed then that is the only direction the player will go towards
+        if self.lastQuadrant in quadrants_available:
+            quadrants_available = [self.lastQuadrant]
+
+        # create an array of possible angles where the player can go
+        available_directions = []
+        for quad in quadrants_available:
+            available_directions += list(range(quad[0], quad[1]))
+        
+        # choose a direction where the player goes
+        direction_deg = random.choice(available_directions)
+        direction = direction_deg * (np.pi / 180)
+
+        # based on the chosen direction, mark it to be repeated
+        for quad in quadrants_available:
+            if quad[0] < direction_deg < quad[1]:
+                self.lastQuadrant = quad
+                break
+
+        # can the player run at different speeds? makes the problem much harder
+        if self.randomSpeeds:
+            dist_covered = random.uniform(0, self.avgDistCovered)
+        else:
+            dist_covered = self.avgDistCovered
+
+        # this is the new player position for this time step
+        self.playerPos += np.array(
+            [
+                dist_covered * np.cos(direction),
+                dist_covered * np.sin(direction),
+            ]
+        )
+
     def rangingGenerator(self) -> np.ndarray:
         """
-        This function returns the erroneous distance of the player to the 4 sensors placed at the 4 corners of the field.
-        Calculates the Eucledian distance between two given points.
+        This function returns the noise-added distance of the player to the 4 sensors placed at the 4 corners of the field.
+        Calculates the Eucledian distance between two given points: current position and each of the receivers.
         """
-        self.whereAreYouRunning()
+        # array to hold all four distances
         distances = []
         for rec_position in self.receiverPos.values():
             distances.append(
@@ -93,6 +227,9 @@ class FieldAssets:
 
     def getPosition(self) -> np.ndarray:
         return self.playerPos
+    
+    def allowRandomSpeeds(self, allow: bool) -> None:
+        self.randomSpeeds = allow
 
 
 def threaded_run():
