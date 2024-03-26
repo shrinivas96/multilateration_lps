17e40fde01e5ba96cd18f5a97a0a9ae64143cc57
shrinivas96 <shrinivasiyengar@live.com>
2024-03-26 18:14:09 +0100

    Changes in KF solution before sending to Lukas  - added some comments to explain what the functions do  - added an if-else block to show the three approaches that were used before

--- kalman_filter.py
+++ kalman_filter.py
@@ -6,13 +6,28 @@ import numpy as np
 
 
 def state_jacobian(state: np.ndarray, l1: float):
+    """
+    Linearised circle equations, to use them as some sort of a state transition matrix. 
+    Two non linear equations used here:
+    (BL rec) x^2 + y^2 = l1^2       => x = sqrt(l1^2 - y^2)
+    (TL rec) x^2 + (y-60)^2 = l2^2  => y = (l1^2 - l2^2 + c1) / c2
+    (c1, c2 are some constants that we get after expanding and substituting.)
+    """
     y = state[1]
     F = np.zeros((2, 2))
     F[0, 1] = y / np.sqrt(l1**2 + y**2)
+    # ^^there is already a mistake here: there is a negative sign missing in the numerator, and y^2 should be negative in denominator
+    # setting them to negative yields NaN or Inf error.
     return F
 
 
 def simple_state_transition(measurement: np.ndarray) -> np.ndarray:
+    """
+    Simple solution to the position estimate based on equations of circles. Gives out two possible positions,
+    but was observed that (x1, y1) was always the plausible one, albeit not the right solution due to presence of noise.
+    This was used as the predicted state for the Prediction step of the Kalman Filter.
+    """
+    # equations have been solved on paper and hard-coded here
     l1, l2, l3, l4 = measurement[:]
     y1 = (l1**2 - l2**2 + 60**2)/120
     y2 = (l4**2 - l3**2 + 60**2)/120
@@ -23,11 +38,21 @@ def simple_state_transition(measurement: np.ndarray) -> np.ndarray:
 
 
 def self_prediction(estimator: ExtendedKalmanFilter) -> ExtendedKalmanFilter:
+    """
+    Self implemented prediction step of the (Extended) Kalman Filter. Removed out of the one proided by the library,
+    because I wanted to have my own state transition jacobian matrix that updates in each time step.
+    """
+    # get state and measurement 
     state = estimator.x
     measurement = estimator.z
+
+    # update state transition matrix based on current state
     estimator.F[0:2, 0:2] = state_jacobian(state, measurement[0])
+
+    # predicted state and covariance
     estimator.x = np.dot(estimator.F, state)
     estimator.P = np.dot(estimator.F, estimator.P).dot(estimator.F.T) + estimator.Q
+
     return estimator
 
  
@@ -41,22 +66,28 @@ if __name__ == "__main__":
 
     # player's path for visualisation
     player_trajectory = np.zeros((initial_position.shape[0], total_iterations))
-    # player_trajectory[:, 0] = initial_position
 
     # estimated trajectory, for visualisation
     initial_guess = np.array([49.0, 23.0, 0, 0])
     est_trajectory = np.zeros((initial_guess.shape[0], total_iterations))
-    # est_trajectory[:, 0] = initial_guess
 
     # kalman filter config
     ekf_estimator = ExtendedKalmanFilter(dim_x=4, dim_z=4)
 
-    # setting a high covariance for motion model because the model is definitely inaccurate
+    # three approaches were tried:
+    # 1: constant velocity model: adding velocity states, and multiply with dt to get position
+    # 2: using solution from equations of circle as prediction step 
+    # 3: linearising circle equations and using that as state transition matrix
+    approach = 2
+
     ekf_estimator.x = initial_guess
     state_transition = 1.0*np.eye(4)
-    state_transition[0, 2] = delta_t
-    state_transition[1, 3] = delta_t  
+    if approach == 1:
+        state_transition[0, 2] = delta_t
+        state_transition[1, 3] = delta_t  
     ekf_estimator.F = state_transition
+
+    # setting a high covariance for motion model because the model is definitely inaccurate
     ekf_estimator.Q = 30.0*np.eye(4)
     ekf_estimator.P = 50.0*np.eye(4)
 
@@ -72,12 +103,17 @@ if __name__ == "__main__":
         ekf_estimator.update(distance_meas, jacobian_ext, distance_function)
         est_trajectory[:, i] = ekf_estimator.x
         
-        # hot-wiring the prediction step: did not work
-        # ekf_estimator.x[0:2] = simple_state_transition(distance_meas)
-        # ekf_estimator = self_prediction(ekf_estimator)
 
-        # run kf prediction step, update player position
-        ekf_estimator.predict()
+        if approach == 1:
+            # run kf prediction step, update player position
+            ekf_estimator.predict()
+        if approach == 2:
+            # sort of "hot-wiring" the prediction step: did not work!!
+            ekf_estimator.x[0:2] = simple_state_transition(distance_meas)
+        if approach == 3:
+            ekf_estimator = self_prediction(ekf_estimator)
+
+        
         obj.alternativeRunning()
     
     # gimme that plot
