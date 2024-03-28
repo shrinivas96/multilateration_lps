Minor corrections, LSO-EKF integration  - Bug in `position_processor.py`: computation of jacobian depended on number of states, while jacobian is only being computed for the first 2 columns.  - In `main.py`: player position should be updated before being saved for visualisation.  - `kalman_filter.py`: Prediction step should be run after player position is updated.  - Removed random module, replaced by numpy random in `generate_ranges.py`.  - First attempt at combining LSO and EKF estimation. Does not show any considerable improvements.

generate_ranges.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 
 # for reproducible results
-np.random.seed(0)
+np.random.seed(155)
 
 class FieldAssets:
     def __init__(self, fieldLength, fieldWidth, initPos=None) -> None:
@@ -73,7 +72,7 @@ class FieldAssets:
             available_directions += list(range(ranges[0], ranges[1]))
 
         # choose a direction where the player goes
-        direction = random.choice(available_directions) * (np.pi / 180)
+        direction = np.random.choice(available_directions) * (np.pi / 180)
 
         # this is the new player position for this time step
         self.playerPos += np.array(
@@ -125,7 +124,7 @@ class FieldAssets:
             available_directions += list(range(quad[0], quad[1]))
 
         # choose a direction where the player goes
-        direction_deg = random.choice(available_directions)
+        direction_deg = np.random.choice(available_directions)
         direction = direction_deg * (np.pi / 180)
 
         # based on the chosen direction, mark it to be duplicated
@@ -136,7 +135,7 @@ class FieldAssets:
 
         # can the player run at different speeds? makes the problem much harder
         if self.randomSpeeds:
-            dist_covered = random.uniform(0, self.avgDistCovered)
+            dist_covered = np.random.uniform(0, self.avgDistCovered)
         else:
             dist_covered = self.avgDistCovered
 
@@ -188,7 +187,7 @@ class FieldAssets:
             available_directions += list(range(quad[0], quad[1]))
         
         # choose a direction where the player goes
-        direction_deg = random.choice(available_directions)
+        direction_deg = np.random.choice(available_directions)
         direction = direction_deg * (np.pi / 180)
 
         # based on the chosen direction, mark it to be repeated
@@ -199,7 +198,7 @@ class FieldAssets:
 
         # can the player run at different speeds? makes the problem much harder
         if self.randomSpeeds:
-            dist_covered = random.uniform(0, self.avgDistCovered)
+            dist_covered = np.random.uniform(0, self.avgDistCovered)
         else:
             dist_covered = self.avgDistCovered
 

kalman_filter.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     player_trajectory[:, 0] = obj.getPosition()
 
     # estimated trajectory, for visualisation
-    initial_guess = np.array([49., 24., 0.25, 0.25])
+    initial_guess = np.array([48., 22., 0.25, 0.25])
     # initial_guess = np.array([1., 1., 0.25, 0.25])
     est_trajectory = np.zeros((initial_guess.shape[0], total_iterations))
     est_trajectory[:, 0] = initial_guess
@@ -54,12 +54,12 @@ if __name__ == "__main__":
             ekf_estimator.update(distance_meas, jacobian_ext, hEucledian_distance_function, hx_args=(obj.receiverPos,))
         est_trajectory[:, i] = ekf_estimator.x
 
-        # run kf prediction step
-        ekf_estimator.predict()
-
         # update player position, save it
         obj.alternativeRunning()
         player_trajectory[:, i] = obj.getPosition()
+        
+        # run kf prediction step
+        ekf_estimator.predict()
 
     # gimme that plot
     plt.figure(figsize=(10, 8))

position_processor.py
@@ -82,7 +82,7 @@ def measurement_jacobian(
         xpi = xp - xi
         ypi = yp - yi
         denominator = np.sqrt(xpi**2 + ypi**2)
-        hJacobian[index, 0:no_of_states] = np.divide([xpi, ypi], denominator)
+        hJacobian[index, 0:2] = np.divide([xpi, ypi], denominator)
     return hJacobian
 
 
@@ -136,7 +136,7 @@ def scipy_least_squares():
     measurement = np.array(obj.rangingGenerator())
 
     initial_guess = np.array([45.0, 20.0])
-    state_res = optimize.least_squares(residual_function, initial_guess, method='lm', args=(measurement,))
+    state_res = optimize.least_squares(residual_function, initial_guess, method='lm', args=(measurement, obj.receiverPos))
     print(state_res.x)
 
 